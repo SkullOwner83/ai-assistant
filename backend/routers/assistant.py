ec4278fe350aa5d482a7de7055261cf8f81cc1e1
Javier Guzmán Cruz <skullowner83@gmail.com>
2025-09-18 00:29:58 -0600

    feat: Extend the file metadata and refactor to generate the chunks and embeddings only if the file has not been stored

--- backend/routers/assistant.py
+++ backend/routers/assistant.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import chromadb
 from uuid import uuid4
@@ -26,9 +27,11 @@ router = APIRouter(
 )
 
 import hashlib
+from datetime import datetime, timezone
 
 def get_hash(file: UploadFile) -> str:
     content = file.file.read()
+    file.file.seek(0)
     hash = hashlib.md5(content).hexdigest()
     return hash
 
@@ -38,24 +41,39 @@ async def ask(question: str = Form(...), conversation_id: Optional[int] = Form(N
     conversation_created = None
 
     if file:
-        document_chunks = await DatasetProcesator.chunk_file(file)
-        embeddings_chunks = await embeddings.get_document_embeddings(document_chunks)
-        texts = [doc.page_content for doc in document_chunks]
         file_hash = get_hash(file)
         exists = chroma_collection.get(where={"source": file_hash})
 
         if not exists["ids"]:
+            document_chunks = await DatasetProcesator.chunk_file(file)
+            embeddings_chunks = await embeddings.get_document_embeddings(document_chunks)
+            texts = [doc.page_content for doc in document_chunks]
+
             chroma_collection.add(
                 documents=texts,
                 embeddings=embeddings_chunks,
-                metadatas=[{'source': file_hash}] * len(texts),
-                ids=[str(uuid4()) for _ in texts]
+                ids=[str(uuid4()) for _ in texts],
+                metadatas=[
+                    {
+                        'chunk_index': i,
+                        'source': file_hash,
+                        'filename': file.filename,
+                        'content_type': file.content_type,
+                        'upload_date': datetime.now(timezone.utc).isoformat(),
+                        'size': getattr(file, "size", None),
+                        'page_count': len(texts)
+                    }
+                    for i in range(len(texts))
+                ]
             )
 
-        query_embedding = embeddings.get_embedding(question)
-        query_embedding = np.array([query_embedding], dtype='float32')
-        results = chroma_collection.query(query_embeddings=query_embedding, n_results=5)
+    query_embedding = embeddings.get_embedding(question)
+    query_embedding = np.array([query_embedding], dtype='float32')
+    results = chroma_collection.query(query_embeddings=query_embedding, n_results=5)
+    
+    if results and results.get('documents') and results['documents'][0]:
         context_text = results['documents'][0][0]
+
         # relevant_chunks = await embeddings.search(question, embeddings_chunks, texts)
         # context_text = relevant_chunks[0]
 
