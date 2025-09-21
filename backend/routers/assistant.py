166eedf67161feb19d7b421362bb67bf9206dfbd
Javier Guzmán Cruz <skullowner83@gmail.com>
2025-09-20 23:37:09 -0600

    feat: Create a fallback in ask endpoint to return the most relevant text founded of dataset when the AI api fails.

--- backend/routers/assistant.py
+++ backend/routers/assistant.py
@@ -1,8 +1,8 @@
 import os
-from typing import Optional
-import numpy as np
-
+from typing import List, Optional
 from fastapi import APIRouter, Depends, Form, UploadFile, status
+from httpx import HTTPError
+from openai import APIError
 from sqlalchemy.orm import Session
 from infraestructure.database import open_connection
 from infraestructure.ia_client import IAClient
@@ -20,23 +20,19 @@ router = APIRouter(
     tags=["assistant"]
 )
 
-
-
 @router.post('/', response_model=AskResponseSchema, status_code=status.HTTP_200_OK)
 async def ask(question: str = Form(...), conversation_id: Optional[int] = Form(None), file: Optional[UploadFile] = None, db_session: Session = Depends(open_connection)) -> dict[str, str]:
-    context_text = None
-    conversation_created = None
-
     if file: await rag_service.process_file(file)
     results = rag_service.search(question)
-    context_text = results[0] if results else None
+    context = "\n\n".join(results) if results else None
+    conversation_created = None
 
-        # relevant_chunks = await embeddings.search(question, embeddings_chunks, texts)
-        # context_text = relevant_chunks[0]
-
-    #prompt = f"Context: {context_text}\n\nQuestion: {question}" if context_text else f"question: {question}"
-    #response = await ai_client.ask(prompt)
-    response = context_text or "No se encontró información relevante."
+    try:
+        prompt = f"Context: {context}\n\nQuestion: {question}" if context else f"question: {question}"
+        response = await ai_client.ask(prompt)
+    except (APIError, HTTPError, TimeoutError) as e:
+        print(f"Error. No se pudo generar una respuesta: {e}")
+        response = context or "No se encontró información relevante."
 
     if not conversation_id:
         conversation_created = chat_service.create_conversation(question[:50], db_session)
@@ -44,7 +40,4 @@ async def ask(question: str = Form(...), conversation_id: Optional[int] = Form(N
 
     chat_service.create_message(question, 'Client', conversation_id, db_session)
     answer = chat_service.create_message(response, 'Server', conversation_id, db_session)
-    return AskResponseSchema(
-        answer=answer,
-        conversation=conversation_created
-    )
+    return AskResponseSchema(answer=answer, conversation=conversation_created)
