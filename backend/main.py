b2a34c967e6d646349d7f30f0246081307e7f102
Javier Guzmán Cruz <skullowner83@gmail.com>
2025-10-03 17:49:02 -0600

    feat: Create an endpoint to check the api health.

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException, Response, status
 from fastapi.middleware.cors import CORSMiddleware
+from infraestructure.database import open_connection
+from sqlalchemy.orm import Session
+from sqlalchemy import text
 from dotenv import load_dotenv
 from routers import *
-from services.rag_service import RAGService
 
 load_dotenv()
 app = FastAPI()
@@ -21,3 +23,15 @@ app.add_middleware(
 @app.get('/')
 async def root():
     return {"message": "AI Assistant"}
+
+@app.get('/health')
+async def health_check(db: Session = Depends(open_connection)):
+    try:
+        db.execute(text('SELECT 1'))
+        return Response(status_code=status.HTTP_200_OK)
+    except Exception as e:
+        raise HTTPException(status_code=503, detail=str(e))
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
