0f9c5a773aace96bcbf14e5e80c2d835c3abcfb3
Javier Guzmán Cruz <skullowner83@gmail.com>
2025-10-05 19:20:36 -0600

    refactor: Separate the Delete Modal in a functional component.

---
+++ frontend/src/components/delete_modal.tsx
@@ -0,0 +1,28 @@
+import type React from "react";
+import ReactMarkdown from "react-markdown";
+
+interface DeleteModalProps {
+    description?: string;
+    onDelete?: () => void;
+    onCancel?: () => void;
+}
+
+export const DeleteModal: React.FC<DeleteModalProps> = ({ description, onDelete, onCancel }) => {
+    return(
+        <div>
+            <h1>Confirmar eliminación</h1>
+            <p>
+                <ReactMarkdown>{description}</ReactMarkdown>
+            </p>
+
+            <div className="Modal-Buttons">
+                <button className="Cancel-Button" onClick={() => onCancel?.()}>Cancelar</button>
+                <button
+                    className="Delete-Button"
+                    onClick={() => {onDelete?.()}}>
+                    Confirmar
+                </button>
+            </div>
+        </div>
+    )
+}
